c16de4079df8c1c3a06a72a26e7ced474ff53b69
devin Du <magicboy317327966@163.com>
2016-10-13 16:29:01 +0800

    新增类型

--- NSUserDefaults+custom.h
+++ NSUserDefaults+custom.h
@@ -10,12 +10,23 @@
 
 @interface NSUserDefaults (custom)
 
-+ (void)dd_setObject:(id)obj ForKey:(NSString *)key;
++ (void)dd_setObject:(id)obj forKey:(NSString *)key;
 
 + (id)dd_objectForKey:(NSString *)key;
 
-+ (void)dd_setBool:(BOOL)b ForKey:(NSString *)key;
++ (void)dd_setBool:(BOOL)b forKey:(NSString *)key;
 
 + (BOOL)dd_boolForKey:(NSString *)key;
 
++ (void)dd_setInteger:(NSInteger)i forKey:(NSString *)key;
+
++ (NSInteger)dd_integerForKey:(NSString *)key;
+
++ (void)dd_setFloat:(float)f forKey:(NSString *)key;
+
++ (float)dd_floatForKey:(NSString *)key;
+
++ (void)dd_setDouble:(double)d forKey:(NSString *)key;
+
++ (double)dd_doubleForKey:(NSString *)key;
 @end
