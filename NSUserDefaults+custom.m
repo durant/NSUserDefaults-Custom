18de29b2f83e3a991fde6dd439f5e4283969bb80
devin Du <magicboy317327966@163.com>
2016-10-13 16:29:49 +0800

    新增类型

--- NSUserDefaults+custom.m
+++ NSUserDefaults+custom.m
@@ -8,28 +8,58 @@
 
 #import "NSUserDefaults+custom.h"
 
-#define ZS_NSUSERDEFAULTS [NSUserDefaults standardUserDefaults]
+#define DD_NSUSERDEFAULTS [NSUserDefaults standardUserDefaults]
 
 @implementation NSUserDefaults (custom)
 
-+ (void)dd_setObject:(id)obj ForKey:(NSString *)key
++ (void)dd_setObject:(id)obj forKey:(NSString *)key
 {
-    [ZS_NSUSERDEFAULTS setObject:obj forKey:key];
+    [DD_NSUSERDEFAULTS setObject:obj forKey:key];
 }
 
 + (id)dd_objectForKey:(NSString *)key
 {
-    return [ZS_NSUSERDEFAULTS objectForKey:key];
+    return [DD_NSUSERDEFAULTS objectForKey:key];
 }
 
-+ (void)dd_setBool:(BOOL)b ForKey:(NSString *)key
++ (void)dd_setBool:(BOOL)b forKey:(NSString *)key
 {
-    [ZS_NSUSERDEFAULTS setBool:b forKey:key];
+    [DD_NSUSERDEFAULTS setBool:b forKey:key];
 }
 
 + (BOOL)dd_boolForKey:(NSString *)key
 {
-    return [ZS_NSUSERDEFAULTS boolForKey:key];
+    return [DD_NSUSERDEFAULTS boolForKey:key];
+}
+
++ (void)dd_setInteger:(NSInteger)i forKey:(NSString *)key
+{
+    [DD_NSUSERDEFAULTS setInteger:i forKey:key];
+}
+
++ (NSInteger)dd_integerForKey:(NSString *)key
+{
+    return [DD_NSUSERDEFAULTS integerForKey:key];
+}
+
++ (void)dd_setFloat:(float)f forKey:(NSString *)key
+{
+    [DD_NSUSERDEFAULTS setFloat:f forKey:key];
+}
+
++ (float)dd_floatForKey:(NSString *)key
+{
+    return [DD_NSUSERDEFAULTS floatForKey:key];
+}
+
++ (void)dd_setDouble:(double)d forKey:(NSString *)key
+{
+    [DD_NSUSERDEFAULTS setDouble:d forKey:key];
+}
+
++ (double)dd_doubleForKey:(NSString *)key
+{
+    return [DD_NSUSERDEFAULTS doubleForKey:key];
 }
 
 
